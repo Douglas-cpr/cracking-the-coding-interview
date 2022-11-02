03f3aeedfa13ed9296a3d91460a03d7cd13df937
Douglas <douglas-cprdev@hotmail.com>
2022-11-02 08:39:27 -0300

    refactor arrayList and add more methods

--- Chap1ArraysAndStrings/ArrayList/ArrayList.cs
+++ Chap1ArraysAndStrings/ArrayList/ArrayList.cs
@@ -1,23 +1,115 @@
+using System.Collections;
+
 namespace Chap1.ArrayList;
 
-public class ArrayList
+public class MyArrayList : IList
 {
-  private int _length = 2;
-  public string[] data;
+  private object[] _items;
 
-  public ArrayList() 
+
+  public MyArrayList() 
   {
-    data = new string[_length];
+    _items = new object[1];
   }
 
-  public void Add(string value) 
+  public object? this[int index]
   {
-    data.Append(value);
-
-    if (data.Length == _length) 
+    get 
     {
-      _length = _length * 2;
-      Array.Resize(ref data, _length);
+      return _items[index];
+    }  
+    set
+    {
+      if (value != null) _items[index] = value;
     }
   }
+
+  public bool IsFixedSize => false;
+
+  public int Count => _items.Length;
+
+  public int Add(object? value)
+  {
+    var size = _items.Length;
+    IncreaseArraySize();
+    _items[size - 1] = value;
+    return size - 1;
+  }
+
+  private void IncreaseArraySize()
+  {
+    object[] tmp = new object[_items.Length + 1];
+    for(int i = 0; i < _items.Length; i++) {
+        tmp[i] = _items[i];
+    }
+    _items = tmp;
+  } 
+
+  public void Clear()
+  {
+    _items = new object[0];
+  }
+
+  public bool Contains(object? value)
+  {
+    return IndexOf(value) != -1;
+  }
+
+  public void CopyTo(Array array, int index)
+  {
+    if (index < 0) throw new IndexOutOfRangeException();
+    for (var i = index; i < _items.Length; i++) 
+    {
+      array.SetValue(_items[i], i);
+    }
+  }
+
+  public int IndexOf(object? value)
+  {
+    for(var i = 0; i < _items.Length; i++)
+    {
+      if (_items[i].Equals(value)) return i;
+    }
+    return -1;
+  }
+
+  public void Insert(int index, object? value)
+  {
+    if (index < 1 && index > _items.Length+1) throw new IndexOutOfRangeException();
+    if (index >= _items.Length) 
+    {
+      Add(value);
+      return;
+    }
+    _items[index] = value;
+  }
+
+  public void Remove(object? value)
+  {
+    var index = IndexOf(value);
+    if (index == -1) return;
+    RemoveAt(index);
+  }
+
+  public void RemoveAt(int index)
+  {
+    var temp = new object[_items.Length - 1];
+    for(int i = 0, j = 0; i < temp.Length; i++, j++)
+    {
+      if (i == index) j++;
+      temp[i] = _items[j];
+    }
+    _items = temp;
+  }
+
+  public IEnumerator GetEnumerator()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool IsReadOnly => throw new NotImplementedException();
+
+  public bool IsSynchronized => throw new NotImplementedException();
+
+  public object SyncRoot => throw new NotImplementedException(); 
 }
--- Chap1ArraysAndStrings/Program.cs
+++ Chap1ArraysAndStrings/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Chap1.ArrayList;
+
 internal class Program
 {
   private static void Main(string[] args)
@@ -8,18 +9,22 @@ internal class Program
     var arr2 = new string[] { "a", "b", "c", "d"};
 
     var merged = merge(arr1, arr2);
-
-    Console.WriteLine(merged);
-
   }
 
 
-  private static ArrayList merge(string[] a, string[] b)
+  private static MyArrayList merge(string[] a, string[] b)
   {
-    var arrayList = new ArrayList();
+    var arrayList = new MyArrayList();
 
     foreach(var str in a) arrayList.Add(str);
     foreach(var str in b) arrayList.Add(str);
+
+    arrayList.Remove("e");
+    arrayList.RemoveAt(1);
+    arrayList.Insert(0, 1);
+    arrayList.Insert(6, 6);
+    arrayList.Insert(7, 7);
+    arrayList.Insert(8, 8);
 
     return arrayList;
   }
