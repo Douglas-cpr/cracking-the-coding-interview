e549de8c33df0faddd21d965c0e50674708ab499
Douglas <douglas-cprdev@hotmail.com>
2022-11-06 14:30:07 -0300

    adding permutation exercises

--- Chap1ArraysAndStrings/ex/exercises.cs
+++ Chap1ArraysAndStrings/ex/exercises.cs
@@ -3,9 +3,16 @@ public class Exercices
 {
   public void Main()
   {
-    var value = "Ta doidao cachoeira";
-    
-    var result = UniqueWithoutDataStructures(value);
+    var uniqueWithoutDataStructuresParam = "Ta doidao cachoeira";
+    UniqueWithoutDataStructures(uniqueWithoutDataStructuresParam);
+
+
+    var permutationParam1 = "aabb";
+    var permutationParam2 = "abba";
+
+    CheckPermutationO2(permutationParam1, permutationParam2);
+    CheckPermutationON(permutationParam1, permutationParam2);
+
   }
 
   public bool UniqueWithoutDataStructures(string value) 
@@ -23,6 +30,52 @@ public class Exercices
       }
       passed.Add(c);
     }
+    return true;
+  }
+
+  public bool CheckPermutationO2(string value1, string value2)
+  {
+    var v1Size = value1.Length;
+    var v2Size = value2.Length;
+
+    if (v1Size != v2Size) return false;
+
+    var char1 = value1.ToCharArray();
+    var char2 = value2.ToCharArray();
+
+    Array.Sort(char1);
+    Array.Sort(char2);
+
+    for(var i = 0; i < char1.Length; i++)
+    {
+      if (char1[i] != char2[i]) return false;
+    }
+
+    return true;
+  }
+
+  public bool CheckPermutationON(string value1, string value2)
+  {
+    var NO_OF_CHARS = 256;
+
+    var v1 = value1.ToCharArray();
+    var v2 = value2.ToCharArray();
+    var count1 = new int[NO_OF_CHARS];
+    var count2 = new int[NO_OF_CHARS];
+
+    for (var i = 0; i < v1.Length && i < v2.Length; i++)
+    { 
+      count1[v1[i]]++;
+      count2[v2[i]]++;
+    }
+
+    if (v1.Length != v2.Length) return false;
+ 
+    for (var i = 0; i < NO_OF_CHARS; i++) 
+    {
+        if (count1[i] != count2[i]) return false;
+    }
+
     return true;
   }
 
