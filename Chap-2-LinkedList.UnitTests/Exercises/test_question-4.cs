431c062f2c5d9bf01ed97f6454420ca08ebe3f15
Douglas <douglas-cprdev@hotmail.com>
2022-12-02 14:38:24 -0300

    adding a better algorithm to question 4 chap 2

--- Chap-2-LinkedList.UnitTests/Exercises/test_question-4.cs
+++ Chap-2-LinkedList.UnitTests/Exercises/test_question-4.cs
@@ -2,25 +2,38 @@ namespace Chap_2_LinkedList.UnitTests;
 
 public class TestQuestion4
 {
-  public Node<int> BuildNodesToTest(List<int> values)
+  private Node<int> BuildNodesToTest(List<int> values)
   {
     var nodes = new Node<int>(values[0]);
     for(var i = 1; i < values.Count; i++) nodes.AppendToTail(values[i]);
     return nodes;
   }
 
+
   [Fact]
-  public void ShouldReturnHeadWithoutModification_WhenDontHaveMiddleNode()
+  public void BadImpShouldReturnHeadWithoutModification_WhenDontHaveMiddleNode()
   {
-    var nodes = BuildNodesToTest(new List<int>() { 1, 15, 10, 5, 6, 2, 11 });
+    var nodes = BuildNodesToTest(new() { 1, 15, 10, 5, 6, 2, 11 });
+    var expectedResponse = BuildNodesToTest(new() { 1, 2, 10, 5, 6, 15, 11 });
+
     Question4.Partition(nodes, 5);
-    var expectedResponse = new Node<int>(1);
-    expectedResponse.AppendToTail(2);
-    expectedResponse.AppendToTail(10);
-    expectedResponse.AppendToTail(5);
-    expectedResponse.AppendToTail(6);
-    expectedResponse.AppendToTail(15);
-    expectedResponse.AppendToTail(11);
+
+    while(nodes != null && expectedResponse != null)
+    {
+      nodes.Data.Should().Be(expectedResponse.Data);
+      nodes = nodes.Next;
+      expectedResponse = expectedResponse.Next;
+    }
+  }
+
+  [Fact]
+  public void OptimizesImpShouldReturnHeadWithoutModification_WhenDontHaveMiddleNode()
+  {
+    var nodes = BuildNodesToTest(new() { 1, 15, 10, 5, 6, 2, 11 });
+    var expectedResponse = BuildNodesToTest(new() { 1, 2, 15, 10, 5, 6, 11 });
+
+    Question4.PartitionOptimized(nodes, 5);
+
     while(nodes != null && expectedResponse != null)
     {
       nodes.Data.Should().Be(expectedResponse.Data);
--- Chap-2-LinkedList/Exercises/question-4.cs
+++ Chap-2-LinkedList/Exercises/question-4.cs
@@ -1,10 +1,19 @@
 namespace Code;
 // 2.4 Partition: Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x. 
 // (IMPORTANT: The partition element x can appear anywhere in the “right partition”; it does not need to appear between the left and right partitions. The additional spacing the example below indicates the partition.)
+
+
+// Note: other possible solution is create 3 LinkedList: 1 to smallest than pivot, 2 to equal pivot, 3 to largest than pivot
+// then merge them
+
 public static class Question4
 {
+    // Space O(N2)
+    // Time O(N2)
     public static void Partition(Node<int> head, int partition)
     { 
+      if (head.Next == null) return;
+
       while (head != null)
       {
         var pointer_aux = head.Next;
@@ -39,6 +48,49 @@ public static class Question4
       node_temp = node_temp.Next;
     }
     node.Data = aux_temp;
+  }
+
+
+    // Space O(N)
+    // Time O(N)
+  public static void PartitionOptimized(Node<int> head, int partition)
+  {
+    var smallest = new Node<int>(-1);
+    var largestOrEqual = new Node<int>(-1);
+    var temp = head;
+
+    while(temp != null)
+    {
+      if (temp.Data < partition)
+      {
+        smallest.AppendToTail(temp.Data);
+      }
+      else 
+      {
+        largestOrEqual.AppendToTail(temp.Data);
+      }
+
+      temp = temp.Next;
+    }
+    
+    // not run with static creators
+    smallest = smallest.Next;
+    largestOrEqual = largestOrEqual.Next;
+
+    while(head != null && largestOrEqual != null)
+    {
+      if (smallest != null)
+      {
+        head.Data = smallest.Data;
+        smallest = smallest.Next;
+      }
+      else 
+      {
+        head.Data = largestOrEqual.Data;
+        largestOrEqual = largestOrEqual.Next;
+      }
+      head = head.Next;
+    }
   }
 
 }
